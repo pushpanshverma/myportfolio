ee7ad96b102b6aabb4e08a06536877bc6fc58d3e
pushpanshverma <66127352+pushpanshverma@users.noreply.github.com>
2023-05-14 15:47:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+$(".PG").click(function(e){
+     e.preventDefault();
+    $(".aboutProject").text("Randomly generate a strong password");
+    $(".all_Project div").css("background","transparent");
+    $(".PG").css("background","purple");
+});
